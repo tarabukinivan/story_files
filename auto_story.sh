d8fd35afeb29e3c542c0c478d60fda9b90953a77
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2024-10-03 03:36:13 +0900

    Update auto_story.sh

--- auto_story.sh
+++ auto_story.sh
@@ -126,19 +126,5 @@ sudo systemctl restart story-testnet-geth.service && sudo journalctl -u story-te
 # enable and start story
 sudo systemctl daemon-reload
 sudo systemctl enable story-testnet.service
- 
-SNAP_RPC="https://story-rpc.tarabukin.work:443"
-LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
-BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)); \
-TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
-
-echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH && sleep 2
-
-sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
-s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
-s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
-s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
-s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"\"|" ~/.story/story/config/config.toml
-
 sudo systemctl start story-testnet.service && sudo journalctl -fu story-testnet.service -o cat
 
