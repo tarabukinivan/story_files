b8872eee2ecf6e9c1782159b0421116978ffa461
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2024-10-03 08:44:34 +0900

    Update auto_story.sh

--- auto_story.sh
+++ auto_story.sh
@@ -57,7 +57,7 @@ cd $HOME
 rm -rf story
 git clone https://github.com/piplabs/story
 cd story
-git checkout v0.10.1
+git checkout v0.9.13
 go build -o story ./client
 sudo mv ~/story/story ~/go/bin/
 
